6cafc19409dafed5a4fc3666be0107609ea33d73
Hira-kiran <hirakiran224@gmail.com>
2023-03-16 09:58:57 -0700

    Initial commit

---
+++ flutter/main.dart
@@ -0,0 +1,28 @@
+void main() {
+// Lecture # 1:
+  // print hello world program
+  print("hello world");
+  print("pakistn");
+
+  // Lec # 2
+// •	What is variable?
+// •	What is constants?
+// •	What is data type (int, double, string)?
+// •	What is identifier?
+// •	How to declare and initialize variables?
+// •	Rules of declaring variable.
+
+  int a = 20;
+  int b = 30;
+  int c = 30;
+  print(a + b + c);
+
+  String name = "Hira";
+
+  /* var c = "flutter";
+  c = "dart"; */
+
+  print("the name of student is $name");
+  print(c);
+  print(a);
+}
