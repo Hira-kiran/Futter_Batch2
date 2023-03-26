0391bf2a132be79692512357b28bbeeb10c6df68
Hira-kiran <hirakiran224@gmail.com>
2023-03-26 10:57:17 -0700

    Lec4

---
+++ dart language/main.dart
@@ -0,0 +1,64 @@
+// •	User input/output
+// •	Logical operators
+// •	Increment/decrement operator
+// •	Assignment operator
+// •	Conditional operators
+
+import 'dart:io';
+
+void main() {
+  // •	User input/output
+  // print("Enter your name:");
+  /*  stdout.write("Enter your name:");
+  var name = stdin.readLineSync();
+  print(name);
+  print("your name is $name");
+  int age = int.parse(stdin.readLineSync()!);
+  print("Your age is $age");
+  double num = double.parse(stdin.readLineSync()!);
+  print(num); */
+// •	Logical operators
+// &&, || , !
+
+  /*  int num1 = int.parse(stdin.readLineSync()!);
+  int num2 = int.parse(stdin.readLineSync()!);
+  bool result = !(num1 < num2);
+  print(result); */
+
+// •	Increment/decrement operator
+// ++, --
+
+  /*  int n = 23;
+  n--;
+  // n = n + 1;
+  print(n); */
+
+// •	Assignment operator
+// +=, -=, *=, /=
+
+  /*  double a = 10;
+  a /= 3;
+  print(a); */
+  //  •	Conditional operators
+
+  // ?, :
+
+  int x = 20;
+  int y = 30;
+  String z = (x > y) ? " condition is true" : "condition is false";
+  print(z);
+}
+
+
+// Assignment
+// 1)	Write a program to calculate the sum of the first 10 natural numbers.
+// 2)	Write a program to enter two numbers and display its sum.
+// 3)	Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+// 4)	Write a program to print following:
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+// 5)	How to convert feet into meter based on user input.
+
