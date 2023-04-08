7bbd0eb634890d2f60001c80c2f9d5343fbafbed
Hira-kiran <hirakiran224@gmail.com>
2023-04-08 11:45:57 -0700

    DART Roadmap

---
+++ Dart RoadMap/main.dart
@@ -0,0 +1,108 @@
+// DART LANGUAGE
+// Lecture # 1:
+// •	What is dart and why we use it?
+// •	How to install Vs code?
+// •	What is Dartpad?
+// •	Install Extensions
+// •	Make first program “hello world”?
+// •	How to write comments?
+// Assignment
+// 1)	Write a program to print all your family members’ name.
+
+
+// Lecture # 2:
+// •	What is variable? 
+// •	What is constants?
+// •	What is data type (int, double, string, num)?
+// •	What is identifier?
+// •	How to declare and initialize variables?
+// •	Rules of declaring variable.
+// Assignment
+// 1)	Write a program to print "Welcome to flutter Mobile application development"
+// 2)	Write a program to perform all arithmetic operations?
+// 3)	Write a program to multiply three decimal number?
+// 4)	Differentiate between variable and dynamic?
+
+
+// Lecture # 3:
+// •	Datatypes(bool, List, Map)
+// •	Differentiate between variable and dynamic
+// •	Relational operators
+// •	Concatenation
+// Assignment	
+// 1)	Write a program in which make rectangle using print statement (*)?
+// 2)	Write a program to perform all relational operators (>, <,>=, <=).
+// 3)	Write a program to subtract two constant numbers.
+// 4)	Write a program to Calculate Area of Circle.
+// 5)	Write a program to Calculate Area of square.
+
+
+// Lecture # 4:
+// •	User input/output
+// •	Logical operators 
+// •	Increment/decrement operator
+// •	Assignment operator
+// •	Conditional operators
+// Assignment
+// 1)	Write a program to calculate the sum of the first 10 natural numbers.
+// 2)	Write a program to enter two numbers and display its sum.
+// 3)	Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+// 4)	Write a program to print following:
+// *
+// ***
+// *****
+// *******
+// *********
+// 5)	How to convert feet into meter based on user input.
+
+
+// Lecture # 5:
+// •	Practice all things that we discuss in previous lectures.
+// •	Using all the concepts make calculator.
+
+
+// Lecture # 6:
+// Conditional Statements
+// •	If statement
+// •	If-else statement
+// •	(if_else_if) statement.
+// •	Switch statement
+// Assignment
+// 1)	Write a program that input two numbers and if second number is square of first using if statement.
+// 2)	Write a program that input marks of three subjects. If the average of marks is more than 80, it display messages “you are above standard” otherwise it display “You are low standard.
+// 3)	Write a program  that input marks of student and display grade according to following criteria:
+// Marks 	Grade
+// >=90	A
+// 80-89	B
+// 70-79	C
+// 60-69	B
+// Below 60	F
+
+// 4)	Write a program that input number of week’s day and display the name of the day. For Example if user enters 1, it display “Monday” and so on using Switch Statement.
+
+
+// Lecture # 7:
+// Practice all conditional statements
+
+
+// Lecture # 8:                  Loops
+// •	For loop
+// •	While loop
+// •	Do while loop
+// Assignment
+// 1)	Write a program that displays counting form 1 to 10 using while loop.
+// 2)	Write a program that displays first five numbers with their square using while loop.
+// 3)	Write a program that display back counting from 10 to 1 using do while loop.
+// 4)	Write a program that display product of all odd numbers from 1 to 10 using for loop.
+
+
+// Lecture # 10: Functions
+// •	Functions
+// •	Functions with parameters
+// •	Async Function
+// •	Future, await 
+// Assignment
+// 1)	Write a program that display a message “Programming makes life interesting” using function.
+// 2)	Write a program that input two number and performs all arithmetic operations using function.
+// 3)	Write a program in which display your name using async function in which wait 30 milliseconds.
+
