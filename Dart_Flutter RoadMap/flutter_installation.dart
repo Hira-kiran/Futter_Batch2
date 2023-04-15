9594fc11dcb1032f060edf0ebcb2a3dddc896611
Hira-kiran <hirakiran224@gmail.com>
2023-04-15 05:53:40 -0700

    flutter installation

---
+++ Dart_Flutter RoadMap/flutter_installation.dart
@@ -0,0 +1,50 @@
+ /*                   Flutter Installation
+Search flutter on google
+Click on first link and click get started
+Select windows/Mac
+In Get the Flutter SDK, Click on zip file
+Downloading is started.
+
+After downloading process
+Click on software and extract it
+Then copy new folder that visible after extract, paste it in C drive.
+
+Then Go to system Search bar and search “Edit Environment variable for your account”
+Then click it and select path option.
+Now go to C drive and click on flutter folder and then click on bin, then copy its path and paste in environment variable path and click OK.
+Now open Command prompt and Run flutter doctor.
+Now Flutter is installed successfully 
+But android and Vs code is not installed
+
+Now we install Android studio
+Go to google and search Android Studio
+Click on first link and click Download Android Studio
+Check agree and click download.
+After installation
+
+Click on software and click next next….
+Select android Virtual device and click next, install
+Click on finish, select “Do not impact setting”, OK
+Don’t sent
+Then Open android studio, next, standard 
+Then click on next, finish
+
+Again Open Command prompt
+Write flutter doctor and Run
+This shows Flutter Plugin and Dart plugin is not installed.
+
+Now we open android studio
+ Click on setting (Configure).
+Select plugins 
+Search Flutter, Click install
+Accept, yes
+Now search Dart
+Click Restart IDE
+Restart
+
+Again Open Command prompt
+Write flutter doctor and run
+This shows android is installed.
+*/
+
+
