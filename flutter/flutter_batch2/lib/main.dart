eb10fb3b768c002b8ef59f764841c94c8e123d3e
Hira-kiran <hirakiran224@gmail.com>
2023-04-16 10:56:06 -0700

    class 1

---
+++ flutter/flutter_batch2/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MYApp());
+}
+
+class MYApp extends StatelessWidget {
+  const MYApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+          title: const Text(
+            "Flutter App",
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.w200),
+          ),
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.purple,
+                  ),
+                  child: const Center(
+                      child: Text(
+                    "Container 1",
+                    style: TextStyle(color: Colors.white),
+                  )),
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.purple,
+                  ),
+                  child: const Center(
+                      child: Text(
+                    "Container 1",
+                    style: TextStyle(color: Colors.white),
+                  )),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.purple,
+                ),
+                child: const Center(
+                    child: Text(
+                  "Container 2",
+                  style: TextStyle(color: Colors.white),
+                )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
